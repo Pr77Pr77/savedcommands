03a4c8e8299605d412a4c8f2a4105b0e4e363f24
Pr77Pr77 <mtla.baur@gmail.com>
2025-10-02 15:40:14 +0200

    First Commit, just the template.

---
+++ src/client/java/de/aliba2468pr77pr77/savedcommands/SavedCommandsClient.java
@@ -0,0 +1,10 @@
+package de.aliba2468pr77pr77.savedcommands;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SavedCommandsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
